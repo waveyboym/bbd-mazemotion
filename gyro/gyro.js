dcfe71a0cbfbc1a829b7ae49da9095b41c731b5e
Michael Todd <michaeltodd981@gmail.com>
2024-07-01 17:45:53 +0200

    Added X Axis

--- gyro/gyro.js
+++ gyro/gyro.js
@@ -1,13 +1,15 @@
 window.addEventListener("load", function () {
-
+    var x = document.getElementById("gyroX");   
     var y = document.getElementById("gyroY");
     var z = document.getElementById("gyroZ");
 
     window.addEventListener('deviceorientation', handleOrientation);
 
     function handleOrientation(event) {
+        const x_val = event.alpha;
         const y_val = event.beta;
         const z_val = event.gamma;
+        x.innerText = "X: " + Math.round(x_val);
         y.innerText = "Y: " + Math.round(y_val);
         z.innerText = "Z: " + Math.round(z_val);
 }
