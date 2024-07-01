b91d2faae44efbeb8e6e4515740ec223e4bdb117
Michael <u21546551@tuks.co.za>
2024-07-01 14:27:13 +0200

    chore: Refactor socket connection port to 3000 and update event handling

--- index.js
+++ index.js
@@ -12,12 +12,20 @@ app.get("/", (_req, res) => {
 
 io.on("connection", (socket) => {
     console.log("A user connected");
-    socket.on("message", (msg) => {
-        console.log("message: " + msg);
-        io.emit("message", msg);
+
+    // Broadcast that the user has connected
+    io.emit("newUser", "A user has connected");
+
+    socket.on("message", (message) => {
+        console.log(`Message: ${message}`);
+        // Broadcast the message to all connected clients except the one that sent the message
+        socket.broadcast.emit("message", message);
     });
+
     socket.on("disconnect", () => {
         console.log("A user disconnected");
+        // Broadcast that the user has disconnected
+        io.emit("removeUser", "A user has disconnected");
     });
 });
 
@@ -25,7 +33,7 @@ io.on("connection", (socket) => {
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port http://localhost:${port}`);
 });
 
 module.exports = app;
