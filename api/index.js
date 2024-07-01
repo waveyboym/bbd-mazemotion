5988385dcea3bc36f34be19b333d84ebe9f6325f
Michael <u21546551@tuks.co.za>
2024-07-01 13:40:14 +0200

    chore: Remove Vercel configuration and Express API files
    This commit removes the Vercel configuration file (vercel.json) and the Express API file (api/index.js) from the repository. These files are no longer needed as we are making changes to the project structure.
    
    The Vercel configuration file is deleted because we are no longer using Vercel for deployment. The Express API file is deleted because we have decided to use a different approach for handling API requests.
    
    Additionally, the package.json file is updated to include the "start" script, which will run the index.js file when the project is started. The "socket.io" dependency is also added to the package.json file.
    
    This commit is part of a series of changes to refactor the project and improve its functionality.

--- api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(55555, () => console.log("Server ready on port 55555."));
-
-module.exports = app;
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get("/", (_req, res) => { 
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on("connection", (socket) => {
+    console.log("A user connected");
+    socket.on("message", (msg) => {
+        console.log("message: " + msg);
+        io.emit("message", msg);
+    });
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+// Use a port that Vercel can dynamically assign
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
