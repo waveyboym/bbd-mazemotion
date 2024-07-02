6dcb9ffd7edced51661ddb85bf2defe320ea9cf0
Michael <u21546551@tuks.co.za>
2024-07-02 10:41:24 +0200

    chore: Update player display in room.html and viewer.html

--- index.js
+++ index.js
@@ -38,18 +38,12 @@ const rooms = {};
 /**
  * rooms = {
  *    maze: [],
- *    blueUsers: [
+ *    Users: [
  *       user1: {
  *          id: 'socketId',
  *          position: { x: 0, y: 0 }
  *       }
- *    ],
- *    redUsers: [
- *      user1: {
- *         id: 'socketId',
- *        position: { x: 0, y: 0 }
- *     }
- *  }
+ *    ]
  */
 
 io.on("connection", (socket) => {
@@ -66,7 +60,7 @@ io.on("connection", (socket) => {
         while(ExistingRoom){
             const roomId = (Math.random() + 1).toString(36).substring(2);
             if(!rooms[roomId]){
-                rooms[roomId] = { maze: [], redUsers: [], blueUsers: []};
+                rooms[roomId] = { maze: [], Users: []};
                 socket.join(roomId);
                 socket.emit('roomCreated', { roomId });
                 console.log(`Room created with ID: ${roomId}`);
@@ -78,31 +72,24 @@ io.on("connection", (socket) => {
     // expected data = roomId
     socket.on('joinRoom', (roomId) => {
         // Check if the room exists and has less than 4 users
-        if (rooms[roomId] && rooms[roomId].redUsers.length + rooms[roomId].blueUsers.length < 4) {
+        if (rooms[roomId] && rooms[roomId].Users.length < 4) {
             socket.join(roomId);
-            // Assign user to red or blue team
-            if (rooms[roomId].redUsers.length <= rooms[roomId].blueUsers.length) {
-                rooms[roomId].redUsers.push({ id: socket.id, position: { x: 0, y: 0 } });
-                socket.emit('roomJoined', { roomId, team: 'red'});
-                console.log(`User joined room with ID: ${roomId} as red`);
-            } else {
-                rooms[roomId].blueUsers.push({ id: socket.id, position: { x: 0, y: 0 } });
-                socket.emit('roomJoined', { roomId, team: 'blue'});
-                console.log(`User joined room with ID: ${roomId} as blue`);
-            }
+            rooms[roomId].Users.push({ id: socket.id, position: { x: 0, y: 0 } });
+            socket.emit('roomJoined', { roomId });
+            console.log(`User joined room with ID: ${roomId}`);
 
-            // Notfiy room of players in the room
-            io.to(roomId).emit('playersInRoom', { redUsers: rooms[roomId].redUsers, blueUsers: rooms[roomId].blueUsers });
+            // Notify room of players in the room
+            io.to(roomId).emit('playersInRoom', { Users: rooms[roomId].Users });
 
             // Notify spectators of players in the room
             if (viewers[roomId]) {
                 viewers[roomId].forEach(viewer => {
-                    socket.to(viewer).emit('playersInRoom', { redUsers: rooms[roomId].redUsers, blueUsers: rooms[roomId].blueUsers });
+                    socket.to(viewer).emit('playersInRoom', { Users: rooms[roomId].Users});
                 });
             }
 
             // Notify room if the game can start
-            if (rooms[roomId].redUsers.length >= 1 && rooms[roomId].blueUsers.length >= 1) {
+            if (rooms[roomId].Users.length >= 2) {
                 io.to(roomId).emit('gameReady');
             }
             else {
@@ -129,7 +116,7 @@ io.on("connection", (socket) => {
             console.log(`Viewer joined room with ID: ${roomId}`);
             socket.emit('roomJoined', { roomId: roomId, team: 'spectator'});
             // notify spectator of players in the room
-            socket.emit('playersInRoom', { redUsers: rooms[roomId].redUsers, blueUsers: rooms[roomId].blueUsers });
+            socket.emit('playersInRoom', { Users: rooms[roomId].Users });
         }
         else {
             socket.emit('roomNotFound');
@@ -153,22 +140,17 @@ io.on("connection", (socket) => {
         ];
 
         // initialize positions of players on the maze
-        rooms[roomId].redUsers.forEach(user => {
+        rooms[roomId].Users.forEach(user => {
             user.position = { x: 1, y: 1 };
         });
 
-        // initialize positions of players on the maze
-        rooms[roomId].blueUsers.forEach(user => {
-            user.position = { x: rooms[roomId].maze.length - 2, y: rooms[roomId].maze.length - 2 };
-        });
-
         // emit gameStarted event and send maze and player positions
-        io.to(roomId).emit('gameStarted', { maze: [], redUsers: rooms[roomId].redUsers, blueUsers: rooms[roomId].blueUsers });
+        io.to(roomId).emit('gameStarted', { maze: rooms[roomId].maze, Users: rooms[roomId].Users});
 
         // emit gameStarted event to viewers
         if (viewers[roomId]) {
             viewers[roomId].forEach(viewer => {
-                socket.to(viewer).emit('gameStarted', { maze: [], redUsers: rooms[roomId].redUsers, blueUsers: rooms[roomId].blueUsers });
+                socket.to(viewer).emit('gameStarted', { maze: [], Users: rooms[roomId].Users});
             });
         }
     });
@@ -176,17 +158,12 @@ io.on("connection", (socket) => {
     // expected data = { roomId, position: { x, y } }
     socket.on('updatePosition', (data) => {
         // Find the user in the room and update their position
-        for (const roomId in rooms) {
-            const redUser = rooms[roomId].redUsers.find(user => user.id === socket.id);
-            const blueUser = rooms[roomId].blueUsers.find(user => user.id === socket.id);
-            if (redUser) {
-                redUser.position = data.position;
-                break;
-            } else if (blueUser) {
-                blueUser.position = data.position;
-                break;
-            }
+        const user = rooms[data.roomId].Users.find(user => user.id === socket.id); // this may cause problems
+        if (!user) {
+            console.log('User not found');
+            return;
         }
+        user.position = data.position; // if js updates the reference or a copy of the object
 
         // Broadcast the updated positions to all users in the room except the one that sent the message
         socket.to(data.roomId).emit('updatePosition', data);
@@ -201,19 +178,14 @@ io.on("connection", (socket) => {
     // expected data = none
     socket.on('disconnect', () => {
         console.log('User disconnected');
-        // Find and remove the user from the room
-        for (const roomId in rooms) {
-            const reduserIndex = rooms[roomId].redUsers.indexOf(socket.id);
-            const blueuserIndex = rooms[roomId].blueUsers.indexOf(socket.id);
-            if (reduserIndex !== -1) {
-                rooms[roomId].redUsers.splice(reduserIndex, 1);
-                if (rooms[roomId].redUsers.length < 1 || rooms[roomId].blueUsers.length < 1) {
-                    io.to(roomId).emit('gameNotReady');
-                }
-            }
-            else if (blueuserIndex !== -1) {
-                rooms[roomId].blueUsers.splice(blueuserIndex, 1);
-                if (rooms[roomId].redUsers.length < 1 || rooms[roomId].blueUsers.length < 1) {
+        
+        // Remove the user from the room
+        for (let roomId in rooms) {
+            const userIndex = rooms[roomId].Users.findIndex(user => user.id === socket.id);
+            if (userIndex !== -1) {
+                rooms[roomId].Users.splice(userIndex, 1);
+                socket.to(roomId).emit('playersInRoom', { Users: rooms[roomId].Users });
+                if (rooms[roomId].Users.length < 2) {
                     io.to(roomId).emit('gameNotReady');
                 }
                 break;
